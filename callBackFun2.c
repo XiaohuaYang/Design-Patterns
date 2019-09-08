5d91e3d2731aee53d54cf89948a2c2bf9ff262c7
XiaohuaYang <38249271+XiaohuaYang@users.noreply.github.com>
2019-09-08 17:03:56 +0800

    函数指针的数组来实现函数调用
    函数指针的数组来实现函数调用

--- callBackFun2.c
+++ callBackFun2.c
@@ -1,18 +1,33 @@
 typedef void (*FP)(char* s);
 
-void Invoke(char* s);
+void InvokeA(char* s);
+void InvokeB(char* s);
+void InvokeC(char* s);
 
 int main(int argc，char* argv[])
 {
-    FP fp;
-    fp=Invoke;              //将Invoke函数的入口地址赋值给fp
-    fp("Hello World!\n");   //函数指针fp实现函数调用
+    void* a[]={f1,f2,f3};   //定义了指针数组，这里a是一个普通指针
+    a[0]("Hello World!\n"); //编译错误，指针数组不能用下标的方式来调用函数
+
+    FP f[]={f1,f2,f3};      //定义一个函数指针的数组，这里的f是一个函数指针
+    f[0]("Hello World!\n"); //正确，函数指针的数组进行下标操作可以进行函数的间接调用
     return 0;
 }
 
-void Invoke(char* s)
+void InvokeA(char* s)
 {
     printf(s);
 }
+
+void InvokeB(char* s)
+{
+    printf(s);
+}
+
+void InvokeC(char* s)
+{
+    printf(s);
+}
+
 
 
