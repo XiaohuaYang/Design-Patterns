1507ce9e7dc0dc61afafa9bf03aba9bee9e8cc04
XiaohuaYang <38249271+XiaohuaYang@users.noreply.github.com>
2019-09-08 16:51:47 +0800

    函数指针fp实现函数调用
    函数指针使用宏定义实现

--- callBackFun2.c
+++ callBackFun2.c
@@ -1,8 +1,10 @@
+typedef void (*FP)(char* s);
+
 void Invoke(char* s);
 
-int main()
+int main(int argc，char* argv[])
 {
-    void (*fp)(char* s);    //声明一个函数指针(fp)        
+    FP fp;
     fp=Invoke;              //将Invoke函数的入口地址赋值给fp
     fp("Hello World!\n");   //函数指针fp实现函数调用
     return 0;
@@ -12,3 +14,5 @@ void Invoke(char* s)
 {
     printf(s);
 }
+
+
